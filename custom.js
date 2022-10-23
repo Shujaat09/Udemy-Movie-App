3960bf27f3a4a371c63413b5dfda72eac8e822df
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-10-23 17:31:06 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/tv/99966/credits?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
@@ -12,7 +12,7 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    showMovies(data.results)
+    showMovies(data.cast)
 }
 
 function showMovies(movies){
